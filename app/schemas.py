4908482f338894970b708040e98780f945339e84
Benjamin Dulcio <121459268+bmdzoe@users.noreply.github.com>
2026-03-27 13:10:33 -0400

    Remove extra blank lines in schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,33 +1,21 @@
 from marshmallow import Schema, fields, validate, validates, ValidationError
-
-
 class RegisterSchema(Schema):
     username = fields.Str(required=True, validate=validate.Length(min=3, max=80))
     email = fields.Email(required=True)
     password = fields.Str(required=True, validate=validate.Length(min=6))
-
-
 class LoginSchema(Schema):
     email = fields.Email(required=True)
     password = fields.Str(required=True)
-
-
 class MovieUpdateSchema(Schema):
     genre = fields.Str(validate=validate.Length(max=200))
     rating = fields.Str(validate=validate.OneOf(["G", "PG", "PG-13", "R", "NC-17", "NR"]))
     release_year = fields.Int(validate=validate.Range(min=1888, max=2100))
     overview = fields.Str()
-
-
 class ReviewSchema(Schema):
     rating = fields.Int(required=True, validate=validate.Range(min=1, max=10))
     body = fields.Str(validate=validate.Length(max=2000))
-
-
 class MovieSearchSchema(Schema):
     title = fields.Str(required=True, validate=validate.Length(min=1, max=200))
-
-
 class MovieFilterSchema(Schema):
     genre = fields.Str()
     rating = fields.Str(validate=validate.OneOf(["G", "PG", "PG-13", "R", "NC-17", "NR"]))
